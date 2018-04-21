c706895603f560e767c284db5eacb94c79b00f14
Marius Lungu <mariuslungu9722@gmail.com>
2018-04-21 15:21:32 +0200

    Added layouts and activity classes for create/join Restaurant activity.

---
+++ app/src/main/java/es/otherperspectiv/myapplication/JoinRestaurantActivity.java
@@ -0,0 +1,13 @@
+package es.otherperspectiv.myapplication;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class JoinRestaurantActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_join_restaurant);
+    }
+}
